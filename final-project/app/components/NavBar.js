64cee4338db058a5b6525212bb9d1bcee5498667
Liza Tulyankina <liza.2806@dyn222169.cc.lehigh.edu>
2024-12-03 18:32:50 -0500

    update

--- final-project/app/components/NavBar.js
+++ final-project/app/components/NavBar.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 
 export const NavBar = () => {
-    return(
-        <nav>
-            <Link href='/'>Home</Link>
-            <Link href='/login'>Login</Link>
-        </nav>
-    )
+  return (
+    <div className="w-full bg-black-100">
+      <nav className="flex justify-center space-x-4 py-2 bg-orange-800 text-white">
+        <Link href='/' className="hover:underline">Home</Link>
+        <Link href='/login' className="hover:underline">Login</Link>
+      </nav>
+    </div>
+  )
 }
