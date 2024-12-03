6e931677de8f1b9b55e3d5e941296e34fe4efbb4
Alexis Guzman <alexisguzman@dyn198104.cc.lehigh.edu>
2024-12-03 16:50:35 -0500

    new

---
+++ final-project/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.js",
+    "./app/**/*.ts",
+    "./app/**/*.jsx",
+    "./app/**/*.tsx",
+    "./app/**/*.mdx",
+    "./components/**/*.js",
+    "./components/**/*.ts",
+    "./components/**/*.jsx",
+    "./components/**/*.tsx",
+    "./context/**/*.js",
+    "./lib/**/*.js",
+    "./likedBooks/**/*.js",
+    "./likedBooks/**/*.jsx",
+    "./login/**/*.js",
+    "./login/**/*.jsx",
+    "./lookup/**/*.js",
+    "./lookup/**/*.jsx",
+    "./recommended/**/*.js",
+    "./recommended/**/*.jsx",
+    "./layout.js",
+    "./page.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
